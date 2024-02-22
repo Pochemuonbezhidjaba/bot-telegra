2d1947ff0146a294f569992a4fb719b63d1c619b
Pochemuonbezhidjaba <90302312+Pochemuonbezhidjaba@users.noreply.github.com>
2024-02-22 14:06:32 +0800

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Привет, Октагон!');
 });
 
+app.get('/static', (req, res) => {
+  const response = { header: 'Hello', body: 'Octagon NodeJS Test' };
+  res.json(response);
+});
+
+app.get('/dynamic', (req, res) => {
+  const { a, b, c } = req.query;
+
+  if (!a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c)) {
+      const errorResponse = { header: 'Error' };
+      res.json(errorResponse);
+  } else {
+      const result = (parseInt(a) * parseInt(b) * parseInt(c)) / 3;
+      const response = { header: 'Calculated', body: result.toString() };
+      res.json(response);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
